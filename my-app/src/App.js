02c635a459c2b5b92e742925dafdd3aba906b1f0
Balakumar3018 <balakumarjkp@gmail.com>
2022-04-01 12:16:05 +0530

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import logo from "./logo.png";
+
+function App() {
+  return (
+    <div className="App">
+       <h2>first commit test from vscode ang github desktop</h2>
+    </div>
+  );
+}
+
+export default App;
